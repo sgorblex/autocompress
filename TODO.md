72e8c10c39155c4f148ca5fa9bf71b0c716b1dd6
Alessandro Clerici <ale.clericil@gmail.com>
2022-01-15 21:00:17 +0100

    first decent/complete version

---
+++ TODO.md
@@ -0,0 +1,9 @@
+# TODO
+- [x] abstract ffmpeg command
+- [x] implement missing flags
+- [x] reduce framerate only if convenient
+- [x] select ffmpeg-bar or ffmpeg automatically
+- [x] substitute --shutdown and --hibernate with a single flag with argument
+- [ ] better logging (also include options)
+- [ ] ipc flag change for some flags
+- [ ] use file descriptor for logfile
--- autocompress.sh
+++ autocompress.sh
@@ -4,17 +4,43 @@
 
 set -e
 
+FFMPEG_PRESET=superfast
+FFMPEG_VIDEO=libx265
+FFMPEG_CRF=31
+FFMPEG_AUDIO=aac
+FFMPEG_AUDIO_BITRATE=64k
+FFMPEG_AUDIO_CHANNELS=1
+
+command -v ffmpeg-bar >/dev/null && FFMPEG_COMMAND=ffmpeg-bar || FFMPEG_COMMAND=ffmpeg
+
+LOGFILE=~/.local/share/autocompress.log
+
+INPUT_DIR=todo
+OUTPUT_DIR=elaborated
+BACKUP_DIR=original
+
 
 USAGE="$0: ffmpeg autocompress script by Sgorblex.
 
 USAGE:
-\t$0 [OPTION]
+$0 [OPTIONS]
 
 OPTIONS:
-\t-s, --shutdown\t\tShutdown computer after finishing
-\t-h, --hibernate\t\tHibernate computer after finishing
-\t--docrop\t\tTry to crop black borders (based on the first seconds)
-\t--help\t\t\tShow this help"
+-o, --output DIR		Output directory (default: $OUTPUT_DIR)
+-i, --input DIR			Input directory (default: $INPUT_DIR)
+-b, --backup DIR		Move the originals in this location (default: $BACKUP_DIR,
+				overridden by -d)
+-c, --crop			Try to crop black borders (based on the first seconds)
+-t, --mtime			Adjust mtime output to match original
+-r RATE, --rate RATE		Lower the framerate to RATE if convenient
+-d, --delete			Delete original after
+-a ACTION, --after ACTION	Execute ACTION upon completing operation (see below)
+-h, --help			Show this help
+
+ACTION is one of
+h, hibernate			Hibernate computer (systemd)
+s, shutdown			Shutdown computer (shutdown)
+n, notify			Send notification (notify-send)"
 
 
 sizeof() {
@@ -22,41 +48,43 @@ sizeof() {
 }
 
 
-tododir=todo
-elaborateddir=elaborated
-originaldir=original
-
-
-OPTS=sh
-LONGOPTS=shutdown,hibernate,help,docrop
-
+OPTS=o:i:b:ctr:da:h
+LONGOPTS=output:,input:,backup:,crop,mtime,rate:,delete,after,help
 PARSED=$(getopt --options=$OPTS --longoptions=$LONGOPTS --name "$0" -- "$@")
 eval set -- "$PARSED"
 
-shutdown="" hibernate=""
 while true
 do
 	case "$1" in
-		-s|--shutdown)
-			shutdown="true"
-			if [ -n $hibernate ]
-			then
-				printf "Pick one: shutdown vs hibernate\n"
-				exit 1
-			fi
+		-o|--output)
+			OUTPUT_DIR="$2"
+			shift
 			;;
-		-h|--hibernate)
-			hibernate="true"
-			if [ -n $shutdown ]
-			then
-				printf "Pick one: shutdown vs hibernate\n"
-				exit 1
-			fi
+		-i|--input)
+			INPUT_DIR="$2"
+			shift
 			;;
-		--docrop)
-			docrop="true"
+		-b|--backup)
+			BACKUP_DIR="$2"
+			shift
 			;;
-		--help)
+		-c|--crop)
+			CROP=true
+			;;
+		-t|--mtime)
+			MTIME=true
+			;;
+		-r|--rate)
+			RATE="$2"
+			;;
+		-d|--delete)
+			DELETE=true
+			;;
+		-a|--after)
+			OPT_AFTER="$2"
+			shift
+			;;
+		-h|--help)
 			printf "%s\n" "$USAGE"
 			exit 0
 			;;
@@ -67,71 +95,106 @@ do
 	shift
 done
 
+[ -d "$OUTPUT_DIR" ]	|| (printf "Invalid output directory: %s.\n"	"$OUTPUT_DIR";	 exit 1)
+[ -d "$INPUT_DIR" ]	|| (printf "Invalid input directory: %s.\n"	"$INPUT_DIR";	 exit 1)
+[ -d "$BACKUP_DIR" ]	|| (printf "Invalid backup directory: %s.\n"	"$BACKUP_DIR";	 exit 1)
 
-cd $(dirname $0)
-mkdir -p "$tododir" "$elaborateddir" "$originaldir"
+if [ "$RATE" -le 0 ] 2>/dev/null; then
+	printf "Invalid framerate: %s.\n" "$RATE"
+	exit 1
+fi
 
-for oldname in "$tododir"/*
+if [ -n "$OPT_AFTER" ]; then
+	case "$OPT_AFTER" in
+		h|hibernate)
+			AFTER='printf "Hibernating in 10 seconds...\n"; sleep 10; systemctl hibernate'
+			;;
+		s|shutdown)
+			AFTER='printf "Shutting down in 10 seconds...\n"; sleep 10; shutdown -P now'
+			;;
+		n|notify)
+			AFTER="notify-send 'autocompress has finished its work. Enjoy!'"
+			;;
+		*)
+			printf "Invalid ACTION for --after. See $0 --help.\n"
+			exit 1
+	esac
+fi
+
+
+
+[ -n "$FFMPEG_PRESET" ] && FFMPEG_OPTIONS="$FFMPEG_OPTIONS -preset $FFMPEG_PRESET"
+[ -n "$FFMPEG_VIDEO" ] && FFMPEG_OPTIONS="$FFMPEG_OPTIONS -c:v $FFMPEG_VIDEO"
+[ -n "$FFMPEG_CRF" ] && FFMPEG_OPTIONS="$FFMPEG_OPTIONS -crf $FFMPEG_CRF"
+[ -n "$FFMPEG_AUDIO" ] && FFMPEG_OPTIONS="$FFMPEG_OPTIONS -c:a $FFMPEG_AUDIO"
+[ -n "$FFMPEG_AUDIO_BITRATE" ] && FFMPEG_OPTIONS="$FFMPEG_OPTIONS -b:a $FFMPEG_AUDIO_BITRATE"
+[ -n "$FFMPEG_AUDIO_CHANNELS" ] && FFMPEG_OPTIONS="$FFMPEG_OPTIONS -ac $FFMPEG_AUDIO_CHANNELS"
+
+
+
+for oldname in $(ls $INPUT_DIR)
 do
-	oldname="${oldname##*/}"
 	newname="${oldname%.*}.mp4"
 
-	printf "Done:\t%s"	"$(ls "$elaborateddir" | wc -l)"
-	printf "Remaining:\t%s"	"$(ls "$tododir" | wc -l)"
-	printf "Old name:\t%s"	"$oldname\nNew name:\t$newname"
-	printf "Start:\t%s"	"$(date +%F_%T)" >> compressing.log
-	printf "Old name:\t%s\nNew name:\t%s\n"	"$oldname" "$newname" >> compressing.log
+	printf "Done:\t%s\n"		"$(ls "$OUTPUT_DIR" | wc -l)"
+	printf "Remaining:\t%s\n"		"$(ls "$INPUT_DIR" | wc -l)"
+	printf "Start:\t%s\n"		"$(date +%F_%T)"		>> $LOGFILE
+	printf "Current file:\t%s\n"	"$oldname"			>> $LOGFILE
+	printf "Compressing video... "					>> $LOGFILE
 
-	printf "Compressing video... " >> compressing.log
-
-	if [ -n $docrop ]; then
-		crop=$(ffmpeg -ss 10 -i "$tododir/$oldname" -t 1 -vf cropdetect -f null - 2>&1 | awk '/crop/ { print $NF }' | tail -1)
-		# ffmpeg -y -i "$tododir/$oldname" -preset superfast -c:v libx265 -crf 31 -c:a aac -b:a 64k -ac 1 -r 24 -vf "$crop" "$elaborateddir/$newname"
-		ffmpeg-bar -y -i "$tododir/$oldname" -preset superfast -c:v libx265 -crf 31 -c:a aac -b:a 64k -ac 1 -r 24 -vf "$crop" "$elaborateddir/$newname"
-	else
-		# ffmpeg -y -i "$tododir/$oldname" -preset superfast -c:v libx265 -crf 31 -c:a aac -b:a 64k -ac 1 -r 24 "$elaborateddir/$newname"
-		ffmpeg-bar -y -i "$tododir/$oldname" -preset superfast -c:v libx265 -crf 31 -c:a aac -b:a 64k -ac 1 -r 24 "$elaborateddir/$newname"
+	if [ -n "$RATE" ]; then
+		currfps=$(ffprobe -v error -select_streams v -of default=noprint_wrappers=1:nokey=1 -show_entries stream=r_frame_rate "$INPUT_DIR/$oldname")
+		currfps=$(( ${currfps%/*}/${currfps#*/} ))
+		if [ "$RATE" -lt $currfps ]; then
+			FFMPEG_OPTIONS="$FFMPEG_OPTIONS -r $RATE"
+		fi
+	fi
+	if [ -n "$CROP" ]; then
+		if cropsettings=$(ffmpeg -ss 10 -i "$INPUT_DIR/$oldname" -t 1 -vf cropdetect -f null - 2>&1 | awk '/crop/ { print $NF }' | tail -1); then
+			FFMPEG_OPTIONS="$FFMPEG_OPTIONS -vf $cropsettings"
+		else
+			printf "Couldn't detect cropping information. Proceeding without cropping.\n"
+			printf "Couldn't detect cropping information. Proceeding without cropping.\n" >> $LOGFILE
+		fi
 	fi
 
-	printf "Done.\n"
-	printf "Done.\n" >> compressing.log
+	eval "$FFMPEG_COMMAND -y -i \"$INPUT_DIR/$oldname\" $FFMPEG_OPTIONS \"$OUTPUT_DIR/$newname\""
 
-	oldsize=$(sizeof "$tododir/$oldname")
-	newsize=$(sizeof "$elaborateddir/$newname")
-	printf "Original file size:\t%s B\nNew file size:\t\t%s B\n" "$oldsize" "$newsize" >> compressing.log
+	printf "Done.\n"
+	printf "Done.\n" >> $LOGFILE
+
+	oldsize=$(sizeof "$INPUT_DIR/$oldname")
+	newsize=$(sizeof "$OUTPUT_DIR/$newname")
+	printf "Original file size:\t%s B\nNew file size:\t\t%s B\n" "$oldsize" "$newsize"	>> $LOGFILE
 
 	if [ $oldsize -le $newsize ]
 	then
 		printf "Compressing was pointless... renaming and moving original with [N], deleting compressed\n"
-		printf "--> [N] - removing compressed\n" >> compressing.log
-		ln "$tododir/$oldname" "$originaldir/$oldname"
-		mv "$tododir/$oldname" "$elaborateddir/${oldname%.*}_[N].${oldname##*.}"
-		rm "$elaborateddir/$newname"
+		printf "%s\n" "--> [N] - removing compressed"	>> $LOGFILE
+		mv "$INPUT_DIR/$oldname" "$OUTPUT_DIR/${oldname%.*}_[N].${oldname##*.}"
+		rm "$OUTPUT_DIR/$newname"
 	else
-		# printf "Compressing was useful! Renaming the new produced video with [C], deleting original\n"
-		printf "Compressing was useful! Renaming the new produced video with [C], moving original\n"
-		printf "--> [C] - removing original\n" >> compressing.log
-		mv "$elaborateddir/$newname" "$elaborateddir/${newname%.mp4}_[C].mp4"
-		# rm "$tododir/$oldname"
-		mv "$tododir/$oldname" "$originaldir"/
+		if [ -n "$MTIME" ]; then
+			touch -cr "$INPUT_DIR/$oldname" "$OUTPUT_DIR/$newname"
+		fi
+		if [ -n "$DELETE" ]; then
+			printf "Compressing was useful! Renaming the new produced video with [C], deleting original\n"
+			printf "%s\n" "--> [C] - deleting original"		>> $LOGFILE
+			rm "$INPUT_DIR/$oldname"
+		else
+			printf "Compressing was useful! Renaming the new produced video with [C], moving original\n"
+			printf "%s\n" "--> [C] - moving original"		>> $LOGFILE
+			mv "$INPUT_DIR/$oldname" "$BACKUP_DIR/"
+		fi
+		mv "$OUTPUT_DIR/$newname" "$OUTPUT_DIR/${newname%.mp4}_[C].mp4"
 	fi
 
 	printf "Work done for $newname\n\n"
-	printf "End:\t$(date +%F_%T)\n\n" >> compressing.log
+	printf "End:\t$(date +%F_%T)\n\n" >> $LOGFILE
 done
 
 printf "Task complete!\n"
 
-
-
-if [ -n $hibernate ]
-then
-		printf "Hibernating in 10 seconds...\n"
-		sleep 10
-		systemctl hibernate
-elif [ -n $shutdown ]
-then
-		printf "Shutting down in 10 seconds...\n"
-		sleep 10
-		shutdown -P now
+if [ -n "$AFTER" ]; then
+	eval "$AFTER"
 fi
